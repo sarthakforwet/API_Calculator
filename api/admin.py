f392fa7bcade2ddec2fcc5f23297ff4d596ab787
samyakjain101 <samyakjain101@gmail.com>
2020-05-28 10:23:54 +0530

    tables created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from api.models import Category, CategoryStream, Api
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(CategoryStream)
+admin.site.register(Api)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+
+class CategoryStream(models.Model):
+    category_id = models.ForeignKey(Category,on_delete=models.CASCADE)
+    stream = models.CharField(max_length=500)
+
+class Api(models.Model):
+    stream = models.ForeignKey(CategoryStream,on_delete=models.CASCADE)
+    nature_of_activity = models.CharField(max_length=750)
+    max_score = models.PositiveSmallIntegerField()
+    self_assesment = models.PositiveSmallIntegerField()
+    verified_api_score = models.PositiveSmallIntegerField()
